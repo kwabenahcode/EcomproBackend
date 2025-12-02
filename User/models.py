59575da308414ce62a7f6f35235400eecb932902
kwabenahcode <mrofori507@gmail.com>
2025-12-02 13:27:36 +0000

    first commit

---
+++ User/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
+
+from .managers import CustomUserManager
+
+# Create your models here.
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(verbose_name="email", max_length=200, unique=True)
+    first_name = models.CharField(max_length=200, blank=True, null=True)
+    last_name = models.CharField(max_length=100, blank=True,null=True)
+    state = models.CharField(max_length=100, blank=True, null=True)
+    address = models.CharField(max_length=100, blank=True, null=True)
+    image = models.ImageField(upload_to="profile_image", blank=True, null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
