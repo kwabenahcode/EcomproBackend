d9ff4aecfd9a853857b8804a4dac5ee41a05a4bc
kwabenahcode <mrofori507@gmail.com>
2025-12-08 20:45:05 +0000

    getting product cat with serializer

--- Product/serializer.py
+++ Product/serializer.py
@@ -1,7 +1,19 @@
 from rest_framework import serializers
 from .models import *
 
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ["category"] 
+
 class GetAllProductSerializer(serializers.ModelSerializer):
+    product_category = serializers.SerializerMethodField()
+
     class Meta:
         model = Product
         fields = "__all__"
+
+    def get_product_category(self, product):
+        result = Product.objects.filter(category=product.category)
+        serializer=ProductSerializer(result, many=True)
+        return serializer.data
