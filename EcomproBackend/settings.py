b5f9d6fc078ce933c290e232cd0594dd77e15a19
kwabenahcode <mrofori507@gmail.com>
2026-02-17 16:08:23 +0000

    fix cors

--- EcomproBackend/settings.py
+++ EcomproBackend/settings.py
@@ -165,8 +165,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:5174",
-    "https://ecompro-frontend.vercel.app/",
-    "https://ecompro-online.vercel.app/",
+    "https://ecompro-frontend.vercel.app",
+    "https://ecompro-online.vercel.app",
 
 ]
 
