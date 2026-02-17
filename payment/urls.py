39fc4748c51b8cfdd3c3d8b43dadd4d99e481745
kwabenahcode <mrofori507@gmail.com>
2026-02-17 18:37:55 +0000

    fix payment callback

--- payment/urls.py
+++ payment/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("initiate_payment/", views.InitiatePaymentAPI.as_view(), name="initiate_payment"),
     path("payment_callback/", views.PaymentCallBackAPI.as_view(), name="payment_callback"),
+    path("payment-status/", views.PaymentCallBackAPI.as_view(), name="payment_status"),
 ]
